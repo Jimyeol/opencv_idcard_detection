04882dd6afb48c5f37bc89ad4401e785df51c244
Jimyeol <economy@coinplug.com>
2020-09-25 15:57:25 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_idcard_detection
+//
+//  Created by Economy on 2020/09/25.
+// create
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
